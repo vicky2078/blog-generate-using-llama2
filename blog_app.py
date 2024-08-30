5da462251d2386df1b98eb7905a4a21b62177148
vicky2078 <vicky98kvc@gmail.com>
2024-08-30 15:56:03 +0530

    Add files via upload

---
+++ blog_app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+
+## Function to get response from Llama 2 model
+
+def getLLamaresponse(input_text,no_words,blog_style):
+
+    #LLama2 Model
+    llm=CTransformers(model='llama-2-13b-chat.ggmlv3.q2_K.bin',
+                      model_type='llama',
+                      config={'max_new_tokens':500,
+                              'temperature':0.01})
+    
+    ## prompt Template 
+
+    template="""
+        Write a blog for {blog_style} job profile for a topic {input_text}
+        within {no_words} words."""
+    
+    prompt=PromptTemplate(input_variables=["blog_style","input_text","no_words"],
+                          template=template)
+    
+    ## Generate the response from the Llama-2 Model
+    response=llm(prompt.format(blog_style=blog_style, input_text=input_text,no_words=no_words))
+    print(response)
+    return response
+
+
+
+st.set_page_config(page_title="Generate Blogs",
+                    page_icon='🤖',
+                    layout='centered',
+                    initial_sidebar_state='collapsed')
+
+st.header("Generate Blogs ")
+
+input_text=st.text_input("Enter the Blog Topic🧾")
+
+## Creating the more columns for additonal two fileds
+
+col1,col2=st.columns([5,5])
+
+with col1:
+    no_words=st.text_input("No words")
+with col2:
+    blog_style=st.selectbox("Writing the blog for",
+                            ('Researcher', 'Data Scientist', 'Common People'),index=0)
+submit=st.button("Generate")
+
+## Final response
+
+if submit:
+    st.write(getLLamaresponse(input_text,no_words,blog_style))
+
+
+
+    
